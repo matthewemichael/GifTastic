3dd34aecb291f17e00e2af2e35f7ffafea867338
Matt Michael <matthewemichael@gmail.com>
2019-08-27 23:50:33 -0500

    can now start and pause gif on click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,10 @@
 var topics = ["Seinfeld", "Always Sunny in Philadelphia", "Game of Thrones", "Breaking Bad", "Sopranos", "Stranger Things", "The Office", "The Simpsons", "Parks and Recreation", "Arrested Development"];
 
 
-function displayShowInfo() {
+function displayTopicGifs() {
 
-    var show = $(this).attr("data-title");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + show + "&apikey=0EU2FV2cPQtQBcIb00uJ4hJlE4GKoGcR&limit=10";
+    var selectedTopic = $(this).attr("data-title");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + selectedTopic + "&apikey=0EU2FV2cPQtQBcIb00uJ4hJlE4GKoGcR&limit=10";
 
     $.ajax({
       url: queryURL,
@@ -17,19 +17,27 @@ function displayShowInfo() {
         var results = response.data;
 
         for (var i = 0; i < results.length; i++) {
-            var showDiv = $("<span>");
-            var p = $("<p>").text("Rating: " + results[i].rating);
-            var showImage = $("<img>");
-            showImage.attr("src", results[i].images.fixed_height.url);
-            showDiv.append(p);
-            showDiv.append(showImage);
-            $("#gifs").prepend(showDiv);
-        }
-          
+            var topicDiv = $("<span>");
+            var p = $("<p>").text("Rating: " + results[i].rating.toUpperCase());
+            var topicImage = $("<img>");
+
+            topicImage.attr("src", results[i].images.fixed_height_still.url);
+            topicImage.attr("data-still", results[i].images.fixed_height_still.url);
+            topicImage.attr("data-animate", results[i].images.fixed_height.url)
+            topicImage.attr("data-state", "still")
+            topicImage.addClass("gif");
+
+
+            topicDiv.append(topicImage);
+            $("#gifs").css({
+                "border" : "1px solid #c6c6c6"
+            });
+            topicDiv.append(p);
+            $("#gifs").prepend(topicDiv);
+        
+        };   
     });
-
-}
-
+}    
 
 function renderButtons() {
 
@@ -44,6 +52,20 @@ function renderButtons() {
     } 
 }
 
-$(document).on("click", ".show", displayShowInfo);
+$("#gifs").on("click", ".gif", function(){
+    
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+            
+});
+
+$(document).on("click", ".show", displayTopicGifs);
 
 renderButtons();
